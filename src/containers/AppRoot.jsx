76ae8ab5a75242eb8892613efcc977a225292141
Sarath <sarath.s.m@accenture.com>
2021-11-10 14:24:46 +0530

    Error boundary added with environment check

--- src/containers/AppRoot.jsx
+++ src/containers/AppRoot.jsx
@@ -5,28 +5,30 @@ import Instruct from '../components/pages/Instruct';
 import Monitor from '../components/pages/Monitor';
 import Prepare from '../components/pages/Prepare';
 import Results from '../components/pages/Results';
-
+import ErrorBoundary from './ErrorBoundary';
 
 const AppRoot = () => {
   return (
     <Layout>
-      <Switch>
-        <Route path='/prepare'>
-          <Prepare />
-        </Route>
-        <Route path='/instruct'>
-          <Instruct />
-        </Route>
-        <Route path='/monitor'>
-          <Monitor />
-        </Route>
-        <Route path='/results'>
-          <Results />
-        </Route>
-        <Route path='*'>
-          <Prepare />
-        </Route>
-      </Switch>
+      <ErrorBoundary>
+        <Switch>
+          <Route path='/prepare'>
+            <Prepare />
+          </Route>
+          <Route path='/instruct'>
+            <Instruct />
+          </Route>
+          <Route path='/monitor'>
+            <Monitor />
+          </Route>
+          <Route path='/results'>
+            <Results />
+          </Route>
+          <Route path='*'>
+            <Prepare />
+          </Route>
+        </Switch>
+      </ErrorBoundary>
     </Layout>
   );
 };
